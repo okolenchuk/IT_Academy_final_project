45bdfb7e0dfadd89e9b638c474940d282b210e6e
okolenchuk <okolenchuk@yandex.ru>
2022-12-17 21:01:38 +0300

    project_0.1.3 add func prepare_photos

---
+++ run.py
@@ -0,0 +1,7 @@
+from scripts.preprocess.preprocess_photos import *
+
+
+prepare_photos(r'D:\EDUCATION\IT_academy\!!! final_project\foto', r'D:\EDUCATION\IT_academy\!!! final_project\foto\1')
+
+
+
--- scripts/preprocess/preprocess_photos.py
+++ scripts/preprocess/preprocess_photos.py
@@ -1,24 +1,32 @@
 from retinaface.pre_trained_models import get_model
 from pathlib import Path
 import cv2
+import json
 
 
 def prepare_photos(foto_path, save_path):
     model = get_model("resnet50_2020-07-20", max_size=2048)
     model.eval()
 
-    with open(r'variables.json', 'a') as file:
+    with open(r'variables.json', 'r') as file:
         d = file.read()
-        d['train_photo_path'] = save_path
+    d = json.loads(d)
+    d['train_photo_path'] = save_path
+
+    with open(r'variables.json', 'w') as file:
+        file.write(json.dumps(d))
 
     counter = 1
+    save_path = str(Path(save_path).joinpath('{}.jpg'))
 
-    for fle in Path(foto_path).glob('*.jpg'):
+    photos = Path(foto_path).glob('*.[jpg][jpeg][png]')
+
+    for fle in photos:
         img = cv2.imread(str(fle))
         faces = model.predict_jsons(img)
         for bb in faces:
             x1, y1, x2, y2 = [int(i) for i in bb['bbox']]
-            side = max(x2 - x1, y2 - y1) // 2
+            side = int(max(x2 - x1, y2 - y1) * 1.2) // 2
             x_c, y_c = [max((x1 + x2) // 2, side), max((y1 + y2) // 2, side)]
             new_image = cv2.resize(img[y_c - side:y_c + side, x_c - side:x_c + side], (512, 512))
             cv2.imwrite(save_path.format(counter), new_image)
