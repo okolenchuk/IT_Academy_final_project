2187bca1881d2830beb366ee2821442aa960c3a7
okolenchuk <okolenchuk@yandex.ru>
2022-12-17 16:53:06 +0300

    project_0.1

--- scripts/inference/generating.py
+++ scripts/inference/generating.py
@@ -1,3 +1,41 @@
+import regex as re
+import torch
+from torch import autocast
+import random
+from prompt_selection import *
 
 
+def generate_prompt_images(prompt: str, pipe, num_samples: int = 2,           #change pipe
+                           guidance_scale=7.5, num_inference_steps=100,
+                           save_path=r'/result/{}.png'):                     #save_path
+    height = 512
+    width = 512
+    g_cuda = torch.Generator(device='cuda')
+    with autocast(g_cuda), torch.inference_mode():
+        images = pipe(
+            prompt,
+            height=height,
+            width=width,
+            num_images_per_prompt=num_samples,
+            num_inference_steps=num_inference_steps,
+            guidance_scale=guidance_scale,
+            generator=g_cuda
+        ).images
+    print(prompt)
+    name = re.sub(r'[^\w]', ' ', prompt)
+    c = 0
+    for img in images:
+        img.save(save_path.format(name[:50] + '_' + str(c + 1)))
+        c += 1
 
+
+def generate_n_images(category, class_name: str, num: int = 10):
+    list_prompts = class_prompts(category)
+    for i in range(num):
+        prompt = random.choice[list_prompts].replace('*', class_name)
+        generate_prompt_images(prompt, num_samples=1)
+
+
+def generate_image_with_random_lexica_prompt(word='', num_samples: int = 2):
+    prompt = random_prompt(word=word)
+    generate_prompt_images(prompt, num_samples)
--- scripts/inference/prompt_selection.py
+++ scripts/inference/prompt_selection.py
@@ -3,6 +3,11 @@ import requests
 import json
 import random
 
+prompts_cat = {'women': r'prompts/female.txt',
+               'girl': r'prompts/female.txt',
+               'man': r'prompts/male.txt',
+               'boy': r'prompts/male.txt'}
+
 
 def random_prompt(word='') -> str:
     if word == '':
@@ -15,3 +20,10 @@ def random_prompt(word='') -> str:
     list_prompt = [text['prompt'] for text in prompts]
     prompt = random.choice(list_prompt)
     return prompt
+
+
+def class_prompts(category) -> list:
+    prompts_file = prompts_cat[category]
+    with open(prompts_file, 'r') as file:
+        list_prompts = file.readlines()
+    return list_prompts
