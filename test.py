87a2926eefe85dda61e889e83e61c629af711172
okolenchuk <okolenchuk@yandex.ru>
2022-12-25 00:35:21 +0300

    project_0.2.1

--- test.py
+++ test.py
@@ -29,25 +29,6 @@ def parse_args(input_args=None):
         required=True,
         help="Path to pretrained model or model identifier from huggingface.co/models.",
     )
-    # parser.add_argument(
-    #     "--pretrained_vae_name_or_path",
-    #     type=str,
-    #     default=None,
-    #     help="Path to pretrained vae or vae identifier from huggingface.co/models.",
-    # )
-    # parser.add_argument(
-    #     "--revision",
-    #     type=str,
-    #     default=None,
-    #     required=False,
-    #     help="Revision of pretrained model identifier from huggingface.co/models.",
-    # )
-    # parser.add_argument(
-    #     "--tokenizer_name",
-    #     type=str,
-    #     default=None,
-    #     help="Pretrained tokenizer name or path if not the same as model_name",
-    # )
     parser.add_argument(
         "--instance_data_dir",
         type=str,
@@ -99,22 +80,10 @@ def parse_args(input_args=None):
     parser.add_argument(
         "--save_infer_steps",
         type=int,
-        default=50,
+        default=100,
         help="The number of inference steps for save sample.",
     )
-    # parser.add_argument(
-    #     "--pad_tokens",
-    #     default=False,
-    #     action="store_true",
-    #     help="Flag to pad tokens to length 77.",
-    # )
-    # parser.add_argument(
-    #     "--with_prior_preservation",
-    #     default=False,
-    #     action="store_true",
-    #     help="Flag to add prior preservation loss.",
-    # )
-    # parser.add_argument("--prior_loss_weight", type=float, default=1.0, help="The weight of prior preservation loss.")
+    parser.add_argument("--prior_loss_weight", type=float, default=1.0, help="The weight of prior preservation loss.")
     parser.add_argument(
         "--num_class_images",
         type=int,
@@ -130,26 +99,13 @@ def parse_args(input_args=None):
         default="text-inversion-model",
         help="The output directory where the model predictions and checkpoints will be written.",
     )
-    # parser.add_argument("--seed", type=int, default=None, help="A seed for reproducible training.")
-    # parser.add_argument(
-    #     "--resolution",
-    #     type=int,
-    #     default=512,
-    #     help=(
-    #         "The resolution for input images, all the images in the train/validation dataset will be resized to this"
-    #         " resolution"
-    #     ),
-    # )
-    # parser.add_argument(
-    #     "--center_crop", action="store_true", help="Whether to center crop images before resizing to resolution"
-    # )
     parser.add_argument("--train_text_encoder", action="store_true", help="Whether to train the text encoder")
-    # parser.add_argument(
-    #     "--train_batch_size", type=int, default=4, help="Batch size (per device) for the training dataloader."
-    # )
-    # parser.add_argument(
-    #     "--sample_batch_size", type=int, default=4, help="Batch size (per device) for sampling images."
-    # )
+    parser.add_argument(
+        "--train_batch_size", type=int, default=4, help="Batch size (per device) for the training dataloader."
+    )
+    parser.add_argument(
+        "--sample_batch_size", type=int, default=4, help="Batch size (per device) for sampling images."
+    )
     parser.add_argument("--num_train_epochs", type=int, default=1)
     parser.add_argument(
         "--max_train_steps",
@@ -157,12 +113,6 @@ def parse_args(input_args=None):
         default=None,
         help="Total number of training steps to perform.  If provided, overrides num_train_epochs.",
     )
-    # parser.add_argument(
-    #     "--gradient_accumulation_steps",
-    #     type=int,
-    #     default=1,
-    #     help="Number of updates steps to accumulate before performing a backward/update pass.",
-    # )
     # parser.add_argument(
     #     "--gradient_checkpointing",
     #     action="store_true",
@@ -174,72 +124,8 @@ def parse_args(input_args=None):
         default=5e-6,
         help="Initial learning rate (after the potential warmup period) to use.",
     )
-    # parser.add_argument(
-    #     "--scale_lr",
-    #     action="store_true",
-    #     default=False,
-    #     help="Scale the learning rate by the number of GPUs, gradient accumulation steps, and batch size.",
-    # )
-    # parser.add_argument(
-    #     "--lr_scheduler",
-    #     type=str,
-    #     default="constant",
-    #     help=(
-    #         'The scheduler type to use. Choose between ["linear", "cosine", "cosine_with_restarts", "polynomial",'
-    #         ' "constant", "constant_with_warmup"]'
-    #     ),
-    # )
-    # parser.add_argument(
-    #     "--lr_warmup_steps", type=int, default=500, help="Number of steps for the warmup in the lr scheduler."
-    # )
-    # parser.add_argument(
-    #     "--use_8bit_adam", action="store_true", help="Whether or not to use 8-bit Adam from bitsandbytes."
-    # )
-    # parser.add_argument("--adam_beta1", type=float, default=0.9, help="The beta1 parameter for the Adam optimizer.")
-    # parser.add_argument("--adam_beta2", type=float, default=0.999, help="The beta2 parameter for the Adam optimizer.")
-    # parser.add_argument("--adam_weight_decay", type=float, default=1e-2, help="Weight decay to use.")
-    # parser.add_argument("--adam_epsilon", type=float, default=1e-08, help="Epsilon value for the Adam optimizer")
-    # parser.add_argument("--max_grad_norm", default=1.0, type=float, help="Max gradient norm.")
-    # parser.add_argument("--push_to_hub", action="store_true", help="Whether or not to push the model to the Hub.")
-    # parser.add_argument("--hub_token", type=str, default=None, help="The token to use to push to the Model Hub.")
-    # parser.add_argument(
-    #     "--hub_model_id",
-    #     type=str,
-    #     default=None,
-    #     help="The name of the repository to keep in sync with the local `output_dir`.",
-    # )
-    # parser.add_argument(
-    #     "--logging_dir",
-    #     type=str,
-    #     default="logs",
-    #     help=(
-    #         "[TensorBoard](https://www.tensorflow.org/tensorboard) log directory. Will default to"
-    #         " *output_dir/runs/**CURRENT_DATETIME_HOSTNAME***."
-    #     ),
-    # )
-    parser.add_argument("--log_interval", type=int, default=10, help="Log every N steps.")
-    parser.add_argument("--save_interval", type=int, default=10_000, help="Save weights every N steps.")
-    parser.add_argument("--save_min_steps", type=int, default=0, help="Start saving weights after N steps.")
-    parser.add_argument(
-        "--mixed_precision",
-        type=str,
-        default=None,
-        choices=["no", "fp16", "bf16"],
-        help=(
-            "Whether to use mixed precision. Choose between fp16 and bf16 (bfloat16). Bf16 requires PyTorch >="
-            " 1.10.and an Nvidia Ampere GPU.  Default to the value of accelerate config of the current system or the"
-            " flag passed with the `accelerate.launch` command. Use this argument to override the accelerate config."
-        ),
-    )
     parser.add_argument("--not_cache_latents", action="store_true", help="Do not precompute and cache latents from VAE.")
-    # parser.add_argument("--hflip", action="store_true", help="Apply horizontal flip data augmentation.")
     parser.add_argument("--local_rank", type=int, default=-1, help="For distributed training: local_rank")
-    # parser.add_argument(
-    #     "--concepts_list",
-    #     type=str,
-    #     default=None,
-    #     help="Path to json containing multiple concepts, will overwrite parameters like instance_prompt, class_prompt, etc.",
-    # )
 
     if input_args is not None:
         args = parser.parse_args(input_args)
@@ -265,34 +151,11 @@ def main(args):
         logging_dir=logging_dir,
     )
 
-    # if args['train_text_encoder'] and args['gradient_accumulation_steps'] > 1 and accelerator.num_processes > 1:
-    #     raise ValueError(
-    #         "Gradient accumulation is not supported when training the text encoder in distributed training. "
-    #         "Please set gradient_accumulation_steps to 1. This feature will be supported in the future."
-    #     )
-    #
-    # if args['seed'] is not None:
-    #     set_seed(args['seed'])
 
-    # if args['concepts_list'] is None:
-    #     args['concepts_list'] = [
-    #         {
-    #             "instance_prompt": args['instance_prompt'],
-    #             "class_prompt": args['class_prompt'],
-    #             "instance_data_dir": args['instance_data_dir'],
-    #             "class_data_dir": args['class_data_dir']
-    #         }
-    #     ]
-    # else:
-    #     with open(args['concepts_list'], "r") as f:
-    #         args['concepts_list'] = json.load(f)
-
-    # if args['with_prior_preservation']:
-    # pipeline = None
-    # #     for concept in args['concepts_list']:
-    # class_images_dir = Path(args.class_data_dir)
-    # class_images_dir.mkdir(parents=True, exist_ok=True)
-    # cur_class_images = len(list(class_images_dir.iterdir()))
+    pipeline = None
+    class_images_dir = Path(args.class_data_dir)
+    class_images_dir.mkdir(parents=True, exist_ok=True)
+    cur_class_images = len(list(class_images_dir.iterdir()))
     #
     # if cur_class_images < args.num_class_images:
     #     torch_dtype = torch.float16
@@ -362,7 +225,7 @@ def main(args):
         args.pretrained_model_name_or_path,
         subfolder="unet",
         revision="fp16",
-        torch_dtype=torch.float32
+        torch_dtype=torch.float16
     )
 
     vae.requires_grad_(False)
@@ -416,7 +279,7 @@ def main(args):
         return batch
 
     train_dataloader = torch.utils.data.DataLoader(
-        train_dataset, batch_size=4, shuffle=True, collate_fn=collate_fn, pin_memory=True
+        train_dataset, batch_size=1, shuffle=True, collate_fn=collate_fn, pin_memory=True
     )
 
     weight_dtype = torch.float16
@@ -427,7 +290,7 @@ def main(args):
     vae.to(accelerator.device, dtype=weight_dtype)
     text_encoder.to(accelerator.device, dtype=weight_dtype)
 
-    if not args['not_cache_latents']:
+    if not args.not_cache_latents:
         latents_cache = []
         text_encoder_cache = []
         for batch in tqdm(train_dataloader, desc="Caching latents"):
@@ -598,12 +461,12 @@ def main(args):
                 optimizer.zero_grad(set_to_none=True)
                 loss_avg.update(loss.detach_(), bsz)
 
-            if not global_step % args.log_interval:
+            if not global_step % 10:
                 logs = {"loss": loss_avg.avg.item(), "lr": lr_scheduler.get_last_lr()[0]}
                 progress_bar.set_postfix(**logs)
                 accelerator.log(logs, step=global_step)
 
-            if global_step > 0 and not global_step % args.save_interval and global_step >= args.save_min_steps:
+            if global_step > 0 and not global_step % 1000:
                 save_weights(global_step)
 
             progress_bar.update(1)
